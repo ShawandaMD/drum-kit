81a4300401d1396cb1297292a750fc6433a90069
ShawandaMD <shawandadavis93@gmail.com>
2021-03-05 13:54:11 -0500

    added the cases for all remaining letters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var buttonArray = document.querySelectorAll(".drum");
 
-for (var i=0; i<buttonArray.length; i++) {
-  buttonArray[i].addEventListener("click", function () {
+for (var i = 0; i < buttonArray.length; i++) {
+  buttonArray[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     switch (buttonInnerHTML) {
       case "w":
@@ -14,7 +14,30 @@ for (var i=0; i<buttonArray.length; i++) {
         tom2.play();
         break;
 
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
 
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
 
       default:
 
