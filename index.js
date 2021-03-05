ce155e28a016066f45638376738074c379f1f492
ShawandaMD <shawandadavis93@gmail.com>
2021-03-05 13:40:22 -0500

    added a swtich statement to eventlistener

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ var buttonArray = document.querySelectorAll(".drum");
 
 for (var i=0; i<buttonArray.length; i++) {
   buttonArray[i].addEventListener("click", function () {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play()
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+          var audio = new Audio('sounds/tom-1.mp3');
+          audio.play();
+
+        break;
+      default:
+
+    }
+
   })
 }
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play()
+// this.style.color = "white";
